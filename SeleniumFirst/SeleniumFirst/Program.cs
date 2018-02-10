1c08216b54566ce4f8b9f2b6f01dce34547daab1
Neous1 <Neous1@users.noreply.github.com>
2018-02-10 18:44:49 -0500

    install selenium via nuget instantiate a chrome driver to navigate to a page and insert a text in targeted field

---
+++ SeleniumFirst/SeleniumFirst/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a reference for our browser
+            IWebDriver driver = new ChromeDriver();
+
+
+            // Navigate to web page
+            driver.Navigate().GoToUrl("https://www.google.com/");
+
+            //Select a page element to navigate to
+            IWebElement element = driver.FindElement(By.Name("q"));
+
+            //Perfom Operation
+            element.SendKeys("This is a test");
+        }
+    }
+}
