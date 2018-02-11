0ea661a4ee286892fe65e2f806309ca9a0043c87
Neous1 <Neous1@users.noreply.github.com>
2018-02-10 20:25:49 -0500

    refactor code and add test attributes

--- SeleniumFirst/SeleniumFirst/Program.cs
+++ SeleniumFirst/SeleniumFirst/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
@@ -10,21 +11,48 @@ namespace SeleniumFirst
 {
     class Program
     {
+
+
+        //create a reference for our browser
+        IWebDriver driver = new ChromeDriver();
+
         static void Main(string[] args)
         {
-            //create a reference for our browser
-            IWebDriver driver = new ChromeDriver();
 
+            
+
+
+        }
+
+        //open brower
+        [SetUp]
+        public void Initialize()
+        {
             // Navigate to web page
             driver.Navigate().GoToUrl("https://www.google.com/");
 
+
+        }
+
+        //Execute the operation
+        [Test] // needed to run the test
+        public void ExecuteTest()
+        {
             //Select a page element to navigate to
             IWebElement element = driver.FindElement(By.Name("q"));
 
             //Perfom Operation
             element.SendKeys("This is a test");
 
+
+        }
+
+        //clean up
+        [TearDown] // closes test
+        public void CleanUp()
+        {
             driver.Close();
+
         }
     }
 }
