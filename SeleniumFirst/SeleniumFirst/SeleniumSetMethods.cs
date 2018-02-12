ae735711ef0e520f7f86431220b0e5150476d3a0
Neous1 <Neous1@users.noreply.github.com>
2018-02-12 18:47:06 -0500

    refactor setMethods to use IwebElements from EAPageObjects

--- SeleniumFirst/SeleniumFirst/SeleniumSetMethods.cs
+++ SeleniumFirst/SeleniumFirst/SeleniumSetMethods.cs
@@ -12,46 +12,21 @@ namespace SeleniumFirst
     class SeleniumSetMethods
     {
 
-        //Enter Text
-        public static void EnterText(string element, string value, How elementType)
+        //elementtype declared in EAPageObject
+        public static void EnterText(IWebElement element, string value)
         {
-            if (elementType == How.Id)
-            {
-                PropertiesCollection.Driver.FindElement(By.Id(element)).SendKeys(value);
-            }
-            if (elementType == How.Name)
-            {
-                PropertiesCollection.Driver.FindElement(By.Name(element)).SendKeys(value);
-            }
+            element.SendKeys(value);
         }
         //Click into a button, checkbox, option etc
-        public static void Click(string element, How elementType)
+        public static void Click(IWebElement element)
         {
-            if (elementType == How.Id)
-            {
-                PropertiesCollection.Driver.FindElement(By.Id(element)).Click();
-            }
-            if (elementType == How.Name)
-            {
-                PropertiesCollection.Driver.FindElement(By.Id(element)).Click();
-            }
+           element.Click(); 
         }
 
-
         //Selecting a dropdown control
-        public static void SelectDropDown(string element, string value, How elementType)
+        public static void SelectDropDown(IWebElement element, string value)
         {
-            if (elementType == How.Id)
-            {
-                //Pass the text clicked in the dropdown as a paramter for SelectElement
-
-               new SelectElement(PropertiesCollection.Driver.FindElement(By.Id(element))).SelectByText(value);
-                //this method is used because the selectDropDown method uses string as an element datatype instead of IWebElement.
-            }
-            if (elementType == How.Name)
-            {
-                new SelectElement(PropertiesCollection.Driver.FindElement(By.Name(element))).SelectByText(value);
-            }
+            new SelectElement(element).SelectByText(value);
         }
     }
 }
